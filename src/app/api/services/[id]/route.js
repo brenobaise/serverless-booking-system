e03396464220062c5f8454f45fa430e24ce9d7a4
Breno Baise <joaobaise@hotmail.com>
2025-01-14 09:07:28 +0000

    Refactored API Routes to use NextRequest/Response instead, added find service by id through dynamic routing

---
+++ src/app/api/services/[id]/route.js
@@ -0,0 +1,33 @@
+import connectToDatabase from "@/lib/mongoose";
+import Service from "@/app/models/Service";
+import { NextRequest, NextResponse } from "next/server";
+export async function GET(req, { params }) {
+  try {
+    await connectToDatabase();
+
+    const { id } = params;
+
+    // Validate the ID format (ObjectID format)
+    if (!id || !id.match(/^[0-9a-fA-F]{24}$/)) {
+      return NextResponse.json(
+        { error: "Invalid service ID" },
+        { status: 400 }
+      );
+    }
+
+    // Fetch the service from the database
+    const service = await Service.findById(id);
+
+    if (!service) {
+      return NextResponse.json({ error: "Service not found" }, { status: 404 });
+    }
+
+    return NextResponse.json(service, { status: 200 });
+  } catch (err) {
+    console.log("Error fetching service:", error.message);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
--- src/app/api/services/route.js
+++ src/app/api/services/route.js
@@ -1,26 +1,38 @@
 import connectToDatabase from "@/lib/mongoose";
 import Service from "@/app/models/Service";
-import e, { json } from "express";
-
+import { NextRequest, NextResponse } from "next/server";
 // GET ../services -> returns an object with all available services
 export async function GET(req) {
-  await connectToDatabase();
+  try {
+    await connectToDatabase();
 
-  const services = await Service.find({ isAvailable: true });
-  return new Response(JSON.stringify(services), { status: 200 });
+    const services = await Service.find({ isAvailable: true });
+    console.log(services);
+    return NextResponse.json(services, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching services:", error.message);
+    return NextResponse.json(
+      { error: "Failed to fetch services" },
+      { status: 500 }
+    );
+  }
 }
 
 //POST ../services -> creates a service based on request.body
+
 export async function POST(req) {
   try {
     await connectToDatabase();
-    const body = await req.json();
 
+    const body = await req.json();
     const newService = await Service.create(body);
-    return new Response(JSON.stringify(newService), { status: 200 });
+
+    return NextResponse.json(newService, { status: 201 });
   } catch (error) {
-    return new Response(JSON.stringify({ error: error.message }), {
-      status: 500,
-    });
+    console.error("Error creating service:", error.message);
+    return NextResponse.json(
+      { error: "Failed to create service" },
+      { status: 500 }
+    );
   }
 }
