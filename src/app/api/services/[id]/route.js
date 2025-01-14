7e0eaec72a19977653327a198b48d66963ccd8dd
Breno Baise <joaobaise@hotmail.com>
2025-01-14 09:57:25 +0000

    Added DELETE by Service ID API Route

--- src/app/api/services/[id]/route.js
+++ src/app/api/services/[id]/route.js
@@ -2,7 +2,7 @@ import connectToDatabase from "@/lib/mongoose";
 import Service from "@/app/models/Service";
 import { NextRequest, NextResponse } from "next/server";
 
-// GET api/services/id
+// GET api/services/:id
 export async function GET(req, { params }) {
   try {
     await connectToDatabase();
@@ -27,6 +27,43 @@ export async function GET(req, { params }) {
     return NextResponse.json(service, { status: 200 });
   } catch (err) {
     console.log("Error fetching service:", error.message);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
+
+// DELETE api/services/:id
+export async function DELETE(req, { params }) {
+  try {
+    await connectToDatabase();
+
+    const { id } = params;
+
+    // Validate the ID format (ObjectID format)
+    if (!id || !id.match(/^[0-9a-fA-F]{24}$/)) {
+      return NextResponse.json(
+        { error: "Invalid Service ID" },
+        { status: 400 }
+      );
+    }
+
+    const deletedService = await Service.findByIdAndDelete(id);
+
+    if (!deletedService) {
+      return NextResponse.json({ error: "Service not Found", status: 404 });
+    }
+
+    return NextResponse.json(
+      { message: "Service deleted successfully", service: deletedService },
+      { status: 200 }
+    );
+  } catch (err) {
+    console.error(
+      `Error deleting Service, ID received: ${params}`,
+      err.message
+    );
     return NextResponse.json(
       { error: "Internal Server Error" },
       { status: 500 }
