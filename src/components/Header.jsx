39b7535f7e855be66fbe539e67b73552c539b03b
Breno Baise <joaobaise@hotmail.com>
2025-01-16 08:59:02 +0000

    Added a search booking method by email, added a logout functionallity

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,9 @@
+import LogoutButton from "./LogoutButton";
+
 export default function Header() {
   return (
     <div className="bg-blue-600 min-h-20 flex justify-start  items-center">
+      <LogoutButton />
       <h1 className="text-bold text-white text-4xl pl-10  ">Our Services</h1>
     </div>
   );
